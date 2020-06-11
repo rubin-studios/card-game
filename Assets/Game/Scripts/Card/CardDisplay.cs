d191881315081294a176f423ccf0f3c4dcb49e79
Jeremy <jeremy.m.legere@gmail.com>
2020-06-11 08:25:46 -0300

    Project Create

---
+++ Assets/Game/Scripts/Card/CardDisplay.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class CardDisplay : MonoBehaviour
+{
+
+    public CardTemplate m_Card;
+
+    public TextMeshPro m_Title;
+    public TextMeshPro m_ExternalDescription;
+    public TextMeshPro m_InternalDescription;
+    public TextMeshPro m_Cost;
+    public TextMeshPro m_Revenue;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_Title.text = m_Card.m_CardName;
+        m_ExternalDescription.text = m_Card.m_CardDescriptionTop;
+        m_InternalDescription.text = m_Card.m_CardDescriptionBottom;
+        m_Cost.text = m_Card.m_CardCost.ToString();
+        m_Revenue.text = m_Card.m_Revenue.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
+
+
+
+
---
+++ Assets/Game/Scripts/Card/CardTemplate.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Card", menuName = "Card")]
+public class CardTemplate : ScriptableObject
+{
+
+    public string m_CardName;
+     [TextArea]
+    public string m_CardDescriptionTop;
+     [TextArea]
+    public string m_CardDescriptionBottom;
+    public int m_CardCost;
+    public int m_Revenue;
+}
